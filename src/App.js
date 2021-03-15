b13033f777d6c3dd18698f9c8d5cea01fb151402
shilpa Malempati <shilpamalempati443@gmail.com>
2021-03-15 18:03:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 
 // class 12: Use map to Create React Components from Arrays of Data
 
-import React from 'react';
+// import React from 'react';
+
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = {items: []}
+//   }
+//  componentWillMount(){
+//     fetch( 'https://jsonplaceholder.typicode.com/todos' )
+//       .then( response => response.json() )
+//       // .then (json => console.log(json))
+//       .then(({todos: items}) => this.setState({items}))
+//   }
+//   filter(e){
+//     this.setState({filter: e.target.value})
+//   }
+//   render(){
+//     let items = this.state.items
+//     if( items && this.state.filter){
+//       items = items.filter( item =>
+//       item.title.toLowerCase()
+//       .includes(this.state.filter.toLowerCase()))
+//     }
+//     return (
+//       <div>
+//         <input type="text"
+//           onChange={this.filter.bind(this)}/>
+//         {items && items.map(item =>
+//           <Todos key={item.title} todos={item} />)}
+//       </div>
+//     )
+//   }
+// }
+
+// const Todos = (props) => <h4>{props.todos.title}</h4>
+
+// export default App;
+
+
+
+ import React from 'react';
 
 class App extends React.Component {
   constructor(){
@@ -9,37 +49,33 @@ class App extends React.Component {
     this.state = {items: []}
   }
  componentWillMount(){
-    fetch( 'https://jsonplaceholder.typicode.com/todos' )
+    fetch( 'https://jsonplaceholder.typicode.com/users')
       .then( response => response.json() )
-      .then (json => console.log(json))
-      .then(({todos: items}) => this.setState({items}))
-    // .then(json => {
-    //     this.setState({ todo: json });
-    //   });
-    
-
+      // .then (json => console.log(json))
+      .then(({results: items}) => this.setState({items}))
   }
   filter(e){
     this.setState({filter: e.target.value})
   }
   render(){
     let items = this.state.items
-    if(this.state.filter){
+    if( items && this.state.filter){
       items = items.filter( item =>
-      item.title.toLowerCase()
+      item.name.toLowerCase()
       .includes(this.state.filter.toLowerCase()))
     }
     return (
       <div>
         <input type="text"
           onChange={this.filter.bind(this)}/>
-        {items.map(item =>
-          <Todos key={item.title} person={item} />)}
+        {items && items.map(item =>
+          <Users key={item.name} users={item} />)}
       </div>
     )
   }
 }
 
-const Todos = (props) => <h4>{props.todos.title}</h4>
+const Users = (props) => <h4>{props.users.name}</h4>
 
 export default App;
+
